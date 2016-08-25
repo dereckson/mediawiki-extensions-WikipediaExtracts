7260ec2d1b5ec79164312a628839af594c16e197
Sophivorus <schenonef@gmail.com>
2016-08-24 23:14:22 -0300

    Improve parser function
    Add named parameters similar to the extension tag

--- WikipediaExtracts.php
+++ WikipediaExtracts.php
@@ -56,17 +56,25 @@ class WikipediaExtracts {
 		}
 	}
 
-	static function onFunctionHook( $parser, $title = null, $sentences = null ) {
-		$title = $title ? $title : $parser->getTitle()->getText();
+	static function onFunctionHook( $parser, $input = null ) {
+		// Defaults
+		$title = $parser->getTitle()->getText();
 		$language = $parser->getTargetLanguage()->getCode();
 		$chars = null;
-		$sentences = $sentences ? $sentences : null;
+		$sentences = null;
 		$limit = null;
 		$intro = null;
 		$plaintext = null;
 		$sectionformat = null;
 		$continue = null;
 		$variant = null;
+
+		// Override with user input
+		$options = WikipediaExtracts::extractOptions( array_slice( func_get_args(), 1 ) );
+		extract( $options );
+		if ( $input ) {
+			$title = $parser->recursiveTagParse( $input );
+		}
 
 		// Query the Wikipedia API
 		$data = array(
@@ -95,5 +103,35 @@ class WikipediaExtracts {
 			$extract = $value->extract;
 			return $extract;
 		}
+	}
+
+	/**
+	 * Converts an array of values in form [0] => "name=value" into a real
+	 * associative array in form [name] => value. If no = is provided,
+	 * true is assumed like this: [name] => true
+	 *
+	 * @param array string $options
+	 * @return array $results
+	 */
+	static function extractOptions( $options ) {
+		$results = array();
+
+		foreach ( $options as $option ) {
+			$pair = explode( '=', $option, 2 );
+			if ( count( $pair ) === 2 ) {
+				$name = trim( $pair[0] );
+				$value = trim( $pair[1] );
+				$results[ $name ] = $value;
+			}
+			if ( count( $pair ) === 1 ) {
+				$name = trim( $pair[0] );
+				$results[ $name ] = true;
+			}
+		}
+		// Now you've got an array that looks like this:
+		// [foo] => "bar"
+		// [apple] => "orange"
+		// [banana] => true
+		return $results;
 	}
 }
